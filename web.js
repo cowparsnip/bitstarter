be122854e1c81fab59994fc0e1283462b5f71620
Scott McRae <scottkmcrae@gmail.com>
2013-07-24 03:23:48 +0000

    modded out web.js with the buf

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
+var buf = fs.readFileSync(index.html);
+
 app.get('/', function(request, response) {
-  response.send('Hello Anna love muffin cuddle tiger!');
+  response.send(buf.toString());
 });
 
 
